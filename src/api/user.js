86c79e7061a19da062b48e7cff23bca822594387
huangmian <1652964519@qq.com>
2024-05-14 16:48:10 +0800

    注册逻辑更新

--- src/api/user.js
+++ src/api/user.js
@@ -33,7 +33,7 @@ export const verifycode = (params) =>
 // 账号注册
 export const userRegist = (params) =>
 	request({
-		url: `${AUTH_API_PREFIX}/users/register`,
+		url: `${USER_API_PREFIX}/students/register`,
 		method: 'post',
 		data: params
 	})
